0b9565b098079013bccb279983af776b8c57bcff
Daniel Gabriele <daniel@system76-pc.localdomain>
2022-03-05 13:20:57 -0500

    do not use --reload in docker run command, required for multiple FastAPI workers

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 # encoding=utf-8
 from setuptools import setup
-if __name__ == '__main__':
-    setup()
+
+
+if __name__ == "__main__":
+    setup(
+        install_requires=[
+            "appyratus",
+            "starlette==0.17.1",
+            "fastapi",
+            "sse_starlette",
+            "uvicorn[standard]",
+            "pexpect",
+        ]
+    )
